f2064cef0bf5f5bc61021fab1e8c79b78323e59d
Anatoli Pavlov <18app81@gmail.com>
2023-08-31 15:28:14 +0300

    Update index.js
    - express setup
    - mongoose setup
    - view engine
    - cookieparser

--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieparser = require('cookie-parser');
+const { authMiddleware } = require('./middlewares/authMiddleware');
+
+const databaseName = 'cookstagram';
+
+const routes = require('./routes');
+
+const app = express();
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use('/static', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieparser());
+app.use(authMiddleware);
+app.use(routes);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
+
+app.listen(3000, () => console.log('The server is listening on port 3000...'));
