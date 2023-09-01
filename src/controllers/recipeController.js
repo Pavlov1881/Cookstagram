a1c715c913977988cbff97b2247820c53bcee4f9
Anatoli Pavlov <18app81@gmail.com>
2023-09-01 14:59:35 +0300

    Update recipeController.js
    add create recipe and dynamic content for catalog

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -4,7 +4,32 @@ const { getErrorMessage } = require('../utils/errorUtils');
 const recipeService = require('../services/recipeService');
 
 
+router.get('/catalog', async (req, res) => {
+    try {
+        const allRecipes = await recipeService.getAll().lean().populate('owner');
 
+        res.render('recipes/catalog', { allRecipes });
+    } catch (error) {
+        res.render('/', { error: getErrorMessage(error) });
+    }
+});
+
+router.get('/add', isLogged, (req, res) => {
+    res.render('recipes/addRecipe');
+});
+
+router.post('/add', isLogged, async (req, res) => {
+    try {
+        const ownerId = req.user._id;
+        const recipeData = { ...req.body, owner: ownerId };
+
+        await recipeService.create(recipeData);
+        res.redirect('/recipes/catalog');
+
+    } catch (error) {
+        res.render('recipes/addRecipe', { error: getErrorMessage(error) });
+    }
+})
 
 
 
