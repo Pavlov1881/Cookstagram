5824783b4c50a88cf7452423ef3afb0ca60e8089
Anatoli Pavlov <18app81@gmail.com>
2023-09-01 11:41:59 +0300

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,15 +11,15 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
 
     try {
-        const { email, password } = req.body;
-        const token = await authService.login(email, password);
+        const { username, password } = req.body;
+        const token = await authService.login(username, password);
 
         res.cookie('auth', token);
         res.redirect('/');
 
     } catch (error) {
 
-        return res.status(404).render('src/auth/login', { error: getErrorMessage(error) });
+        return res.status(404).render('auth/login', { error: getErrorMessage(error) });
     }
 
 
@@ -32,13 +32,13 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
 
     try {
-        const { email, username, password, confirmPassword } = req.body;
-        const token = await authService.register(email, username, password, confirmPassword);
+        const { username, email, password, repeatPassword } = req.body;
+        const token = await authService.register(username, email, password, repeatPassword);
 
         res.cookie('auth', token);
         res.redirect('/');
     } catch (error) {
-        res.status(400).render('src/auth/register', { error: getErrorMessage(error) })
+        res.status(400).render('auth/register', { error: getErrorMessage(error) })
     }
 
 });
